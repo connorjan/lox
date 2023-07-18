16810bed132c3f4f89761509bdc7be6392d8ad36
Connor Goldberg <connor@connorgoldberg.com>
2023-07-18 01:35:43 -0400

    Clearing files to start fresh

--- LoxError.py
+++
@@ -1,3 +0,0 @@
-class LoxError:
-    hadError = False
-    hadRuntimeError = False
--- RuntimeException.py
+++
@@ -1,7 +0,0 @@
-from Token import Token
-
-class RuntimeException(Exception):
-
-    def __init__(self, token: Token, message: str):
-        self.message = message
-        self.token = token
